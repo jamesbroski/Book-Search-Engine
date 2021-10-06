f3a7349d8dfee7e84f0df9ea2554a935c7d29f5a
James Broski <james.broski.5@gmail.com>
2021-10-05 18:15:40 -0600

    setting up typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,12 @@
 const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type Mutation{}
+    type User {}
+    type Book {}
+    type BookInput {}
+    type Query {}
+    type Auth {}
+`;
+
+module.exports = typeDefs;
