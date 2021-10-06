c73782c8c61f3d2d723395cd01495092af43b366
James Broski <james.broski.5@gmail.com>
2021-10-05 18:43:34 -0600

    refactored components

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,9 @@
 // see SignupForm.js for comments
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import { loginUser } from "../utils/API";
+// import { loginUser } from "../utils/API";
+import { useMutation } from "@apollo/react-hooks";
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
@@ -49,7 +50,7 @@ const LoginForm = () => {
     }
 
     setUserFormData({
-      // username: "",
+      username: "",
       email: "",
       password: "",
     });
